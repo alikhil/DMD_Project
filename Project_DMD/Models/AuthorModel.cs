3b85c8b8f0a941987f9f8612b9856910b7a7e421
alikhil <alikhil@mail.ru>
2015-09-22 21:21:03 +0300

    Added AuthorModel

---
+++ Project_DMD/Models/AuthorModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_DMD.Models
+{
+    public class AuthorModel
+    {
+        /// <summary>
+        /// Author id
+        /// </summary>
+        public int AuthorId { get; set; }
+
+        /// <summary>
+        /// Author's Name and Last Name
+        /// </summary>
+        public string Name { get; set; }
+
+    }
+}
