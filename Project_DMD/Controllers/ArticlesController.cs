ed2bb35e48024f3c686fc95c68080949a171000e
alikhil <alikhil@mail.ru>
2015-09-22 22:00:16 +0300

    Added Controller for working with Articles

---
+++ Project_DMD/Controllers/ArticlesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Project_DMD.Models;
+using Project_DMD.Classes;
+
+namespace Project_DMD.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private IDataRepository Repository = new DataRepository();
+
+        // GET: Articles
+        public ActionResult Index()
+        {
+            return View(Repository.GetArticles());
+        }
+
+        // GET: Articles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = Repository.GetArticle(id.Value);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // GET: Articles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articles/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ArticleId,Url,Title,Summary,Published,Updated,Venue,DOI")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Add(article);
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+        // GET: Articles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = Repository.GetArticle(id.Value);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Articles/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ArticleId,Url,Title,Summary,Published,Updated,Venue,DOI")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Update(article);
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+        // GET: Articles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = Repository.GetArticle(id.Value);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Article article = Repository.GetArticle(id);
+            Repository.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Repository.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
