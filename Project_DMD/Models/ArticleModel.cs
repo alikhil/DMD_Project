0fa601f1e28e027fc4f639737acf936a4fb8695b
alikhil <alikhil@mail.ru>
2015-09-22 21:21:18 +0300

    Added ArticleModel

---
+++ Project_DMD/Models/ArticleModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_DMD.Models
+{
+    class ArticleModel
+    {
+        /// <summary>
+        /// Id of an article
+        /// </summary>
+        public int ArticleId { get; set; }
+
+        /// <summary>
+        /// Link to the article in arxiv
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Title of an article
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Short description
+        /// </summary>
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// Date of publishing an article
+        /// </summary>
+        public DateTime Published { get; set; }
+
+        /// <summary>
+        /// Date of updating article
+        /// </summary>
+        public DateTime Updated { get; set; }
+
+        /// <summary>
+        /// Conference or journal name
+        /// </summary>
+        public string Venue { get; set; }
+
+        /// <summary>
+        /// DOI
+        /// </summary>
+        public string DOI { get; set; }
+    }
+
+}
