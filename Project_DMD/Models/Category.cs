eb4c1bc8020c3264949749d8aea9700e4f0cce40
alikhil <alikhil@mail.ru>
2015-10-13 20:50:33 +0300

    Adds Category and Keyword

---
+++ Project_DMD/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_DMD.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Project_DMD/Models/Keyword.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_DMD.Models
+{
+    public class Keyword
+    {
+        public int KeywordId { get; set; }
+        public string Keyword { get; set; }
+    }
+}
