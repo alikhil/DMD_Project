d09a4fd7e32fb41e8f1525d161994e9fe379e3e1
alikhil <alikhil@mail.ru>
2015-10-31 16:06:02 +0300

    Adds ags attributes in ActionHistory Model

--- Project_DMD/Models/ActionHistory.cs
+++ Project_DMD/Models/ActionHistory.cs
@@ -1,22 +1,30 @@
 ﻿using System;
+using Project_DMD.Attributes;
 
 
 namespace Project_DMD.Models
 {
+    [AgsModel]
     public class ActionHistory
     {
+        [AgsPrimary]
         public int ActionHistoryId { get; set; }
         
+        [AgsAttribute]
         public DateTime ActionDate { get; set; }
 
+        [AgsAttribute]
         public ActionType ActionDone { get; set; }
 
+        [AgsAttribute]
         public int ArticleId { get; set; }
 
+        [AgsAttribute(Int = true)]
         public string UserId { get; set; }
 
+        [AgsForeign(Name = "UserId",TableName = "Client")]
         public AppUser User { get; set; }
-
+        [AgsForeign(Name = "ArticleId",TableName = "Article")]
         public Article Article { get; set; }
     }
 
