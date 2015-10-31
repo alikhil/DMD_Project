762bd5bdd2cde88c9909eaab169e4ad43e46e2bc
alikhil <alikhil@mail.ru>
2015-10-31 12:47:54 +0300

    Adds ags attributes in article and author models

--- Project_DMD/Models/Author.cs
+++ Project_DMD/Models/Author.cs
@@ -2,24 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Project_DMD.Attributes;
 
 namespace Project_DMD.Models
 {
+    [AgsModel]
     public class Author
     {
         /// <summary>
         /// Author id
         /// </summary>
+        [AgsPrimary]
         public int AuthorId { get; set; }
 
         /// <summary>
         /// Author's Name and Last Name
         /// </summary>
+        [AgsAttribute]
         public string AuthorName { get; set; }
 
         /// <summary>
         /// Articles published by this author
         /// </summary>
+        [AgsForeign(Name = "AuthorId", TableName = "ArticleAuthors")]
         public ICollection<Article> PublishedArticles { get; set; }
 
         public Author() { }
